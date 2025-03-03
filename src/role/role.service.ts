9da98358d7959806338d99d920a8cee97777e1ad
Angel Magaquian <angelmagakhian@gmail.com>
2025-03-03 16:00:52 -0300

    fix import url

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Role } from 'src/schemas/role.schema';
-import { Permission } from 'src/schemas/permission.schema';
+import { Role } from '.../schemas/role.schema';
+import { Permission } from '.../schemas/permission.schema';
 
 @Injectable()
 export class RoleService {
