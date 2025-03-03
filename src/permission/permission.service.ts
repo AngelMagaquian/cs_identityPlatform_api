146a73fad1b2b7174198f84c75086005b4777bcd
Angel Magaquian <angelmagakhian@gmail.com>
2025-03-03 16:01:55 -0300

    fix import url

--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Permission } from '.../schemas/permission.schema';
+import { Permission } from '../schemas/permission.schema';
 
 @Injectable()
 export class PermissionService {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../schemas/user.schema';
-import { Role } from '.../schemas/role.schema';
+import { Role } from '../schemas/role.schema';
 import * as bcrypt from 'bcryptjs';
 
 @Injectable()
